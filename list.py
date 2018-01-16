733bb3fbf7173c94d59def8aee6fa0a3ef841b60
dlrhkdghdlk <dlrhkdghdlk@gmail.com>
2018-01-16 16:54:12 +0900

    list_practice

---
+++ list.py
@@ -0,0 +1,86 @@
+def add():
+    
+num1=int(input('num1:'))
+num2=int(input('num2:'))
+result= num1+num2
+print(result)
+
+def sub():
+num1=int(input('num1:'))
+num2=int(input('num2:'))
+result= num1-num2
+print(result)
+
+def multiply():
+    
+num1=int(input('num1:'))
+num2=int(input('num2:'))
+result= num1*num2
+print(result)
+
+def devide():
+    
+num1=int(input('num1:'))
+num2=int(input('num2:'))
+result= num1+num2
+print(result)
+
+
+def add():
+    
+num1=int(input('num1:'))
+num2=int(input('num2:'))
+if num1==0 or /num2==0:
+    print('error')
+    return
+result= num1/num2
+print(result)
+
+def devide_1():
+    
+num1=int(input('num1:'))
+num2=int(input('num2:'))
+if num2==0:
+    print('error')
+    return
+result=num1%num2
+
+def devide_2():
+num1=int(input('num1:'))
+num2=int(input('num2:'))
+if 
+
+
+
+
+while True :
+    print('Menu')
+    print('----')
+    print('1.add')
+    print('2:sub')
+    print('3:multifly')
+    print('4:divide')
+    print('5.')
+    print('6.')
+    print('7.')
+    print('8.stop')
+    sel=int(input(':'))
+
+if(sel == 1):
+    add()
+elif(sell == 2):
+    sub()
+elif(sell == 3):
+    multiply()
+elif(sell == 4):
+    divide()
+elif(sell == 5):
+    ()
+elif(sell == 6):
+    ()
+elif(sell == 7):
+    ()
+elif(sell == 8):
+    break
+else:
+    print('Wrong input,please input again')
